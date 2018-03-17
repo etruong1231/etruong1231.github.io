bde80e213b3348b02e285e797e54fb1c95242bfd
etruong1231 <etruong7292@gmail.com>
2018-03-16 22:24:52 -0700

    Update style.js

--- resources/js/style.js
+++ resources/js/style.js
@@ -15,6 +15,7 @@
         }
     });
 
+
     // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function () {
         $('.navbar-collapse').collapse('hide');
@@ -50,6 +51,21 @@
     }, {
         offset: '50%'
     });
+    is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+    is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+    is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+    is_safari = navigator.userAgent.indexOf("Safari") > -1;
+    is_opera = navigator.userAgent.indexOf("Presto") > -1;
+    is_mac = (navigator.userAgent.indexOf('Mac OS') != -1);
+    is_windows = !is_mac;
+
+    if (is_chrome && is_safari){
+      is_safari=false;
+    }
+
+    if (is_safari || is_windows){
+      $('body').css('-webkit-text-stroke', '0.5px');
+    }
 
 
 })(jQuery);
